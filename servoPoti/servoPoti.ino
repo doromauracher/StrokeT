b692e1fd3c7e3daac9bf2ee155715cfc3a9bb969
Dorothea Mauracher <dorothea.mauracher@gmail.com>
2022-12-11 11:37:02 +0100

    add floatMap function to map poti values to degrees

--- servoPoti/servoPoti.ino
+++ servoPoti/servoPoti.ino
@@ -4,6 +4,11 @@ Servo myservo;
 int pot = 2;
 int value;
 
+float floatMap(float x, float in_min, float in_max, float out_min, float out_max) {
+  return (x - in_min) * (out_max - out_min) / (in_max - in_min) + out_min;
+}
+
+
 void setup() {
   myservo.attach(2);
   Serial.begin(9600);
@@ -11,7 +16,7 @@ void setup() {
 
 void loop() {
   value = analogRead(pot);
-  value = map (value, 0, 1023, 0, 180);
+  value = floatMap (value, 0, 1023, 0, 180); //map
   myservo.write(value);
   delay(5);
 }
